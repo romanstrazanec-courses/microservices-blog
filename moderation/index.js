269d6a6e3eb4b3a06b440888cfff171cd2c049f0
roman.strazanec <strazanec.roman@gmail.com>
2021-05-09 15:49:27 +0200

    blog app

---
+++ moderation/index.js
@@ -0,0 +1,29 @@
+const app = require('express')()
+const axios = require('axios')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.post('/events', async (req, res) => {
+    const {type, data} = req.body
+
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved'
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        })
+    }
+
+    res.send({})
+})
+
+app.listen(4003, () => {
+    console.log('Listening on port 4003...')
+})
