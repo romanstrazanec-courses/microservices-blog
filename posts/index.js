1a4a9a4688eb1c743a59540f99e34e7548bf6438
roman.strazanec <strazanec.roman@gmail.com>
2021-05-10 00:54:22 +0200

    k8s

--- posts/index.js
+++ posts/index.js
@@ -9,11 +9,7 @@ const posts = {}
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
-
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
     const id = randomBytes(4).toString('hex')
     const {title} = req.body
 
@@ -21,7 +17,7 @@ app.post('/posts', async (req, res) => {
         id, title
     }
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: {
             id, title
